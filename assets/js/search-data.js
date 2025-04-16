2476b7633d3f8198417e486260bc02afadab4c72
davejacobson12 <78111094+davejacobson12@users.noreply.github.com>
2025-04-16 14:15:10 +0000

    Deploying to gh-pages from @ davejacobson12/davejacobson12.github.io@80779ff6df730d0f686162911201a7e65e77e010 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -501,14 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%61%76%65%6A%61%63%6F%62%73%6F%6E%31%32@%67%6D%61%69%6C.%63%6F%6D# %79%6F%75%72 %65%6D%61%69%6C %61%64%64%72%65%73%73", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/davejacobson12", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/dkjacobson-bioinformatician", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0003-1164-8024", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=D5Clc2kAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
